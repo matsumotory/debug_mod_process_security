8eeae8c7175a20d7903b5012835815bf8f413ba8
matsumotory <matsu1229@gmail.com>
2019-11-11 14:55:23 +0900

    Add gdb

--- misc/provision.sh
+++ misc/provision.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 sudo dnf -y update && \
-sudo dnf -y install gcc make httpd httpd-devel pkgconfig libcap-devel redhat-rpm-config && \
+sudo dnf -y install gcc make httpd httpd-devel pkgconfig libcap-devel redhat-rpm-config gdb && \
 sudo dnf clean all && \
 sudo rm -rf /var/cache/dnf
 
